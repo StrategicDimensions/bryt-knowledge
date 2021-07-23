9a8103e71ff5ba2fc6e673b2b09918e087ae6129
Hardik Gajera <hardik@strategicdimensions.co.za>
2021-07-23 10:35:51 +0530

    [Add] New Module website_sale_inherit

---
+++ website_sale_inherit/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Website Sale Inherit",
+    "version": "13.0.1.0.0",
+    "category": "website",
+    "summary": "",
+    "author": "",
+    "license": "AGPL-3",
+    "depends": ["base","website_sale"],
+    "data": [
+        "views/template_inherit.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
